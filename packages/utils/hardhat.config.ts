a4af1dd9731f9bd8c878dd9c816ea9e6ceec878a
wiasliaw <wiasliaw@protonmail.com>
2021-12-01 22:23:37 +0800

    feat(utils): add utils lib

---
+++ packages/utils/hardhat.config.ts
@@ -0,0 +1,51 @@
+import path from 'path';
+import dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+const config: HardhatUserConfig = {
+  // default network
+  defaultNetwork: 'hardhat',
+
+  // network config
+  networks: {
+    hardhat: {
+      throwOnCallFailures: true,
+      throwOnTransactionFailures: true,
+    },
+  },
+
+  // solidity config
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.0',
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+          evmVersion: 'berlin',
+        },
+      },
+    ],
+  },
+
+  // repository config
+  paths: {
+    sources: path.resolve(__dirname, 'contracts'),
+    tests: path.resolve(__dirname, 'tests'),
+    cache: path.resolve(__dirname, './dist/.cache'),
+    artifacts: path.resolve(__dirname, 'dist/artifacts'),
+  },
+
+  // typechain
+  typechain: {
+    outDir: 'dist/types',
+    target: 'ethers-v5',
+  },
+};
+
+export default config;
