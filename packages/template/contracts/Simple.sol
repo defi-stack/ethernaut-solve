f0521450697522de459a976f3035fd5fbf939a9c
wiasliaw <wiasliaw@protonmail.com>
2021-12-01 21:23:56 +0800

    feat(.): development initialization

---
+++ packages/template/contracts/Simple.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+contract Simple {
+    uint256 private _val;
+
+    function get() external view returns (uint256) {
+        return _val;
+    }
+
+    function set(uint256 val_) external {
+        _val = val_;
+    }
+}
